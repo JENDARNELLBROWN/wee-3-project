4b8679bdc410346119a8706f48bc8ef872fb3f1a
Jennifer Brown <jendarnellbrown@gmail.com>
2017-06-15 09:24:35 -0400

    week 3 project -, 3rd version

---
+++ Calc-2/main.js
@@ -0,0 +1,53 @@
+var valueString = "";
+var valueNum = 0;
+var valueOper = "";
+let btnList = document.querySelectorAll(".squares");
+
+for (let i = 0; i < btnList.length; i++) {
+    let btn = btnList[i];
+    btn.addEventListener("click", function (e) {
+        var input;
+        var answer = document.getElementById("answer");
+        input = e.target.innerHTML;
+        switch (input) {
+            case "+":
+            case "-":
+            case "X":
+            case "/":
+            case "=":
+                if (valueOper != "") {
+                    switch (valueOper) {
+                        case "+":
+                            valueNum = valueNum + Number(valueString);
+                            break;
+                        case "-":
+                            valueNum = valueNum - Number(valueString);
+                            break;
+                        case "/":
+                            valueNum = valueNum / Number(valueString);
+                            break;
+                        case "X":
+                            valueNum = valueNum * Number(valueString);
+                            break;
+                        default:
+                    }
+                    answer.innerHTML = valueNum;
+                }
+                else {
+                    valueNum = valueString;
+                }
+                valueString = "";
+                valueOper = input;
+                break;
+            case "C":
+                valueOper = "";
+                valueNum = "";
+                valueString = "";
+                answer.innerHTML = "";
+                break;
+            default:
+                valueString += input;
+                answer.innerHTML = valueString;
+        }
+    });
+}
